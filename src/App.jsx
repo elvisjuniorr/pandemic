f80f857bb31f65e542fb64dff44e8f334fdded1f
Elvis Júnior <elvisjunior994@gmail.com>
2025-06-03 01:04:02 -0300

    feat: instructions-interface finalizada - closes #7

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
               <p>4</p>
             </section>
             <section className='instructions'>
-              <p>*********PLAY*********</p>
+              <p>*****PLAY*****</p>
               <p>1. Do 4 actions</p>
               <p>2. Draw 2 cards</p>
               <p>*Resolve any epidemics</p>
