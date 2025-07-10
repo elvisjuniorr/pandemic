55d917618b660a029fad97ea6213213794197f5e
Tales-Piotrowski <143123013+Tales-Piotrowski@users.noreply.github.com>
2025-07-10 15:42:19 -0300

    Update game_logic.js

--- src/game_logic.js
+++ src/game_logic.js
@@ -258,12 +258,3 @@ class Board {
             [array[i], array[j]] = [array[j], array[i]];
         }
     }
-
-    sampleArray(array, size) {
-        const shuffled = [...array];
-        this.shuffleArray(shuffled);
-        return shuffled.slice(0, size);
-    }
-}
-
-export { City, Player, Board };
